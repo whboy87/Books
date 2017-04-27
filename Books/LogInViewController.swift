c0fc66fba8df50e5ce0e3854aeca066ccb04bfea
SDS-012 <sds-012@70-12-246-127.pools.spcsdns.net>
2017-04-27 14:46:26 +0900

    protocol

--- Books/LogInViewController.swift
+++ Books/LogInViewController.swift
@@ -8,15 +8,23 @@
 
 import UIKit
 
+protocol LogInProtocol {
+    func completedLogIn(name: String)
+}
+
 class LogInViewController: UIViewController {
 
+    var delegate: LogInProtocol? = nil
+    
     @IBOutlet var nameTextField: UITextField!
     
     @IBAction func confirmLogIn(_ sender: Any) {
+        if let del = delegate, let name = nameTextField.text {
+            del.completedLogIn(name: name)
+        }
         
+        self.dismiss(animated: true, completion: nil)
     }
-    
-    var delegate: ViewController? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
