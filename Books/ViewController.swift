d7b6b8621fe854d593d24f8fcd72daa4efd4bda0
SDS-012 <sds-012@70-12-246-127.pools.spcsdns.net>
2017-04-27 13:48:15 +0900

    table

--- Books/ViewController.swift
+++ Books/ViewController.swift
@@ -5,23 +5,22 @@
 //  Created by SDS-012 on 2017. 4. 27..
 //  Copyright © 2017년 SDS-012. All rights reserved.
 //
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         openAlert()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     func openAlert() {
         let alert = UIAlertController(title: "환영합니다.", message: "Books 앱을 시작합니다.", preferredStyle: UIAlertControllerStyle.alert)
         
@@ -51,4 +50,3 @@ class ViewController: UIViewController {
     }
     
 }
-
