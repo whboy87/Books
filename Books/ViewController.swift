6fadf63f41229b3a804ed02801954c46d50c0e8e
SDS-012 <sds-012@70-12-246-127.pools.spcsdns.net>
2017-04-27 11:59:55 +0900

    alert

--- Books/ViewController.swift
+++ Books/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        openAlert()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +22,28 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func openAlert() {
+        let alert = UIAlertController(title: "환영합니다.", message: "Books 앱을 시작합니다.", preferredStyle: UIAlertControllerStyle.alert)
+        
+        let okAction = UIAlertAction(title: "확인", style: UIAlertActionStyle.default, handler: nil)
+        
+        //let cancelAction = UIAlertAction(title: "취소", style: UIAlertActionStyle.cancel, handler: {
+        //    (action: UIAlertAction) -> () in
+        //   print(action.title!)
+        //    self.view.backgroundColor = UIColor.red
+        //})
+        
+        let cancelAction = UIAlertAction(title: "취소", style: UIAlertActionStyle.cancel) {
+            (action: UIAlertAction) -> () in
+            print(action.title!)
+            self.view.backgroundColor = UIColor.red
+        }
+        
+        alert.addAction(okAction)
+        alert.addAction(cancelAction)
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
 }
 
